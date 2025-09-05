ad47758739673b264ad78ed2129be281e5ab14f0
RonaldoN71 <ronaldonandeibam7@gmail.com>
2025-09-05 13:09:52 +0530

    new commit

--- server/index.js
+++ server/index.js
@@ -1,13 +1,8 @@
 
-import cors from "cors";
 import app from "./src/app.js";
 
 const PORT = process.env.PORT || 3000;
 
-
-app.use(
-  cors({})
-);
 
 app.listen(PORT, () => {
   console.log(` PDF Processing Server running on port ${PORT}`);
